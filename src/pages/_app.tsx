df766e6855b1d6e5e6bd97480fed747a43b785d3
“mihir <90671722+mihir-neosoft@users.noreply.github.com>
2022-06-23 11:45:40 +0530

    initial setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../client/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
